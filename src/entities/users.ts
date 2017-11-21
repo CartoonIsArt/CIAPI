7d4e6242cc38d0a992e6400f19150b59e9f92b7d
cocoapi <eksrns22tp@gmail.com>
2017-11-21 20:31:17 +0900

    document & user relation

--- src/entities/users.ts
+++ src/entities/users.ts
@@ -1,8 +1,13 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+import { Column, Entity, JoinColumn, OneToMany, PrimaryGeneratedColumn } from "typeorm"
+import Documents from "./documents"
 
 /* 유저정보 테이블 스키마 */
 @Entity()
 export default class Users {
+
+  @OneToMany(type => Documents, document => document.author)
+  @JoinColumn()
+  public document: Documents[]
 
   /* Users table pk */
   @PrimaryGeneratedColumn()
