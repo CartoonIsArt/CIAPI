b6d9663ad2f3c6d572f230b76ce156077cd18745
cocoapi <eksrns22tp@gmail.com>
2017-11-29 21:48:03 +0900

    Users 인자 수정
    유저 읽기정보에 나와있는 데이터를 기본으로 유저 테이블Column구성.
    
    Passwd와 email이 유저 생성엔 존재하는데, 이에 대한 답변 바람

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -10,27 +10,48 @@ export const Get = async (ctx, next) => {
     .find({ relations: ["profileImage"] })
 }
 
-/* firstname을 POST 인자로 받아 DB에 저장함. */
+/* fullname을 POST 인자로 받아 DB에 저장함. */
 export const Post = async (ctx, next) => {
   /* POST 인자를 data변수로 받음 */
   const data = ctx.request.body
 
-  /* firstname이 인자로 들어왔을 경우 */
-  if (data.firstname !== undefined) {
+  /* fullname이 인자로 들어왔을 경우 */
+  if (data.fullname !== undefined) {
 
     /* DB 커넥션풀에서 커넥션을 하나 가져옴. */
     const conn: Connection = getConnection()
 
     /* Users 테이블 ORM 인스턴스 생성 */
     const user: Users = new Users()
-    user.firstname = data.firstname
-    if (data.profileImage !== undefined){
-      const profile: Files = new Files()
-      profile.file = data.profileImage
-      profile.savedPath = "MIKI"
-      await conn.manager.save(profile)
-      user.profileImage = profile
-    }
+    user.fullname = data.fullname
+
+    /*프로필 이미지를 DB에 포함 및 relation을 구성 */
+    const profile: Files = new Files()
+    profile.file = data.profileImage
+    profile.savedPath = "MIKI"
+    await conn.manager.save(profile)
+    user.username = data.username
+    user.profileImage = profile
+    user.dateOfBirth = data.dateOfBirth
+    user.department = data.department
+    user.studentNumber = data.studentNumber
+    user.nTh = data.nTh
+    user.profileText = data.profileText
+    user.phoneNumber = data.phoneNumber
+    user.favoriteComic = data.favoriteComic
+    user.favoriteCharacter = data.favoriteCharacter
+
+    /* 회원 가입시 기본적으로 고정값 초기화 할 것들 */
+    user.numberOfComments = 0
+    user.numberOfDocuments = 0
+    user.numberOfLikes = 0
+    user.isActivated = false
+    user.isAnon = false
+    user.isGraduated = false
+    user.isRegularMember = false
+    user.isStaff = false
+    user.isSuperUser = false
+
     /* DB에 저장 - 비동기 */
     await conn.manager.save(user)
 
@@ -38,8 +59,8 @@ export const Post = async (ctx, next) => {
     ctx.body = user
   }
   else {
-    /* firstname이 인자에 없을 경우 400에러 리턴 */
-    ctx.throw(400, "firstname required")
+    /* fullname이 인자에 없을 경우 400에러 리턴 */
+    ctx.throw(400, "fullname required")
   }
 }
 export const Delete =  async (ctx, next) => {
