d5ea2c6aa6563e5670a70fd30938379b45caab42
droplet92 <lhs1438@naver.com>
2021-03-21 18:22:13 +0900

    - 컨트롤러 문제 수정 - 사용하지 않는 DELETE method 삭제 - request 및 response 형식 추가

--- src/controllers/comment.ts
+++ src/controllers/comment.ts
@@ -6,48 +6,55 @@ import User from "../entities/user"
 /* 해당 댓글 GET */
 export const GetOne = async (ctx, next) => {
   const conn: Connection = getConnection()
+  const { commentId } = ctx.params
 
-  try{
+  try {
     const comment: Comment = await conn
-    .getRepository(Comment)
-    .findOne(ctx.params.id, {
-      relations: [
-        "author",
-        "author.profileImage",
-        "rootDocument",
-        "likedUsers",
-        "comments",
-        "rootComment",
-      ]})
+      .getRepository(Comment)
+      .findOne(commentId, {
+        relations: [
+          "author",
+          "author.profileImage",
+          "rootDocument",
+          "likedUsers",
+          "comments",
+          "rootComment",
+        ]
+      })
 
+    /* GET 성공 응답 */
+    ctx.response.status = 200
     ctx.body = comment
   }
-  catch (e){
+  catch (e) {
     ctx.throw(400, e)
   }
-
-  /* GET 성공 응답 */
-  ctx.response.status = 200
 }
 
 /* 댓글 POST */
 export const Post = async (ctx, next) => {
   const conn: Connection = getConnection()
   const comment: Comment = new Comment()
-  const data = ctx.request.body.data
+  const { data } = ctx.request.body
   let documentId: number = null
+  const tokenUser = ctx.state.token.user
 
-  try{
-    /* 세션의 유저와 relation 설정 */
-    comment.author = ctx.state.token.user
+  try {
+    /* 유저 불러오기 */
+    const user: User = await conn
+      .getRepository(User)
+      .findOne(tokenUser.id)
+
+    /* 유저와 relation 설정 */
+    comment.author = user
 
     /* commentId를 인자로 전달하면 대댓글 relation 설정 */
     if (typeof(data.commentId) === "number") {
       const parent: Comment = await conn
-      .getRepository(Comment)
-      .findOne(data.commentId, {
-        relations: ["rootDocument"],
-      })
+        .getRepository(Comment)
+        .findOne(data.commentId, {
+          relations: ["rootDocument"],
+        })
 
       comment.rootComment = parent
       documentId = parent.rootDocument.id
@@ -55,9 +62,8 @@ export const Post = async (ctx, next) => {
 
     /* 게시글과 relation 설정 */
     const document: Document = await conn
-    .getRepository(Document)
-    .findOneOrFail(documentId
-      ? documentId : Number(data.documentId))
+      .getRepository(Document)
+      .findOneOrFail(documentId || Number(data.documentId))
 
     comment.rootDocument = document
 
@@ -65,113 +71,83 @@ export const Post = async (ctx, next) => {
     comment.id = data.id
     comment.createdAt = data.created_at
     comment.content = data.text
+    comment.likedUsers = []
 
     /* 댓글 작성자의 댓글 수 1 증가 */
     ++(comment.author.commentsCount)
 
-    comment.likedUsers = []
-
     await conn.manager.save(comment.author)
     await conn.manager.save(comment)
-  }
-  catch (e){
-    if (e.message ===
-    "Cannot read property 'user' of undefined"){
-      ctx.throw(401, e)
-    }
-    ctx.throw(400, e)
-  }
 
-  /* POST 성공 응답 */
-  ctx.body = comment
-  ctx.response.status = 200
-}
-
-/* 해당 댓글 DELETE */
-export const DeleteOne =  async (ctx, next) => {
-  const conn: Connection = getConnection()
-  const leaver: User = await conn.getRepository(User).findOne(0)
-
-  try {
-    /* DB에서 댓글 불러오기 */
-    const comment: Comment = await conn
-    .getRepository(Comment)
-    .findOne(ctx.params.id, {
-      relations: [
-        "author",
-        "likedUsers",
-      ]})
-
-    /* 댓글 작성자의 댓글 수 1 감소 */
-    --(comment.author.commentsCount)
-    await conn.manager.save(comment.author)
-
-    /* 탈퇴한 유저 relation */
-    comment.author = leaver
-    await conn.manager.save(comment)
+    /* POST 성공 응답 */
+    ctx.response.status = 200
+    ctx.body = comment
   }
   catch (e) {
     ctx.throw(400, e)
   }
-
-  /* DELETE 성공 응답 */
-  ctx.response.status = 204
 }
 
 /* 해당 댓글 좋아요 GET */
 export const GetLikes = async (ctx, next) => {
   const conn: Connection = getConnection()
+  const { commentId } = ctx.params
 
   try{
     const comment: Comment = await conn
-    .getRepository(Comment)
-    .findOne(ctx.params.id, {
-      relations: [
-        "likedUsers",
-        "likedUsers.profileImage",
-      ]})
+      .getRepository(Comment)
+      .findOne(commentId, {
+        relations: [
+          "likedUsers",
+          "likedUsers.profileImage",
+        ]
+      })
 
-    ctx.body = comment.likedUsers
+    /* GET 성공 응답 */
+    ctx.response.status = 200
+    ctx.body = { likedUsers: comment.likedUsers }
   }
-  catch (e){
+  catch (e) {
     ctx.throw(400, e)
   }
-
-  /* GET 성공 응답 */
-  ctx.response.status = 200
 }
 
 /* 해당 댓글 좋아요 POST */
 export const PostLikes = async (ctx, next) => {
   const conn: Connection = getConnection()
+  const { commentId } = ctx.params
+  const tokenUser = ctx.state.token.user
 
   try {
     /* DB에서 댓글 불러오기 */
     const comment: Comment = await conn
-    .getRepository(Comment)
-    .findOne(ctx.params.id, {
-      relations: ["likedUsers"],
-    })
+      .getRepository(Comment)
+      .findOne(commentId, {
+        relations: ["likedUsers"],
+      })
 
-    /* 세션 유저 불러오기 */
-    const user: User = ctx.state.token.user
+    /* 유저 불러오기 */
+    const user: User = await conn
+      .getRepository(User)
+      .findOne(tokenUser.id, {
+        relations: ["profileImage"],
+      })
 
-    /* 세션의 유저와 좋아요 relation 설정 */
-    comment.likedUsers.push(user)
+    /* 유저와 좋아요 relation 설정 */
     ++(user.likedCommentsCount)
+    comment.likedUsers.push(user)
 
     await conn.manager.save(comment)
     await conn.manager.save(user)
 
     /* POST 성공 응답 */
-    ctx.body = comment.likedUsers
     ctx.response.status = 200
+    ctx.body = {
+      likedUsers: comment.likedUsers,
+      user,
+    }
   }
   catch (e) {
-    if (e.message ===
-    "Cannot read property 'user' of undefined"){
-      ctx.throw(401, e)
-    }
     ctx.throw(400, e)
   }
 }
@@ -179,35 +155,43 @@ export const PostLikes = async (ctx, next) => {
 /* 해당 댓글 좋아요 DELETE */
 export const CalcelLikes = async (ctx, next) => {
   const conn: Connection = getConnection()
+  const { commentId } = ctx.params
+  const tokenUser = ctx.state.token.user
 
   try {
     /* DB에서 댓글 불러오기 */
     const comment: Comment = await conn
-    .getRepository(Comment)
-    .findOne(ctx.params.id)
+      .getRepository(Comment)
+      .findOne(commentId, {
+        relations: ["likedUsers"],
+      })
 
-    /* 세션 유저 불러오기 */
-    const user: User = ctx.state.token.user
+    /* 유저 불러오기 */
+    const user: User = await conn
+      .getRepository(User)
+      .findOne(tokenUser.id, {
+        relations: ["profileImage"],
+      })
 
-    /* 세션의 유저와 좋아요 relation 해제 */
+    /* 유저와 좋아요 relation 해제 */
     await conn
-    .createQueryBuilder()
-    .relation(Comment, "likedUsers")
-    .of(comment)
-    .remove(user)
+      .createQueryBuilder()
+      .relation(Comment, "likedUsers")
+      .of(comment)
+      .remove(user)
 
-    /* 세션 유저의 댓글 좋아요 수 1 감소 */
+    /* 유저의 댓글 좋아요 수 1 감소 */
     --(user.likedCommentsCount)
     await conn.manager.save(user)
+
+    /* DELETE 성공 응답 */
+    ctx.response.status = 200
+    ctx.body = {
+      likedUsers: comment.likedUsers.filter(x => x.id != user.id),
+      user,
+    }
   }
   catch (e) {
-    if (e.message ===
-    "Cannot read property 'user' of undefined"){
-      ctx.throw(401, e)
-    }
     ctx.throw(400, e)
   }
-
-  /* DELETE 성공 응답 */
-  ctx.response.status = 204
 }
