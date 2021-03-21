8cbfcffa3675fce6ee964c7c3d8b521b8b5308fa
droplet92 <lhs1438@naver.com>
2021-03-21 21:31:21 +0900

    타임라인 검색 및 마이 페이지 타임라인 구현 - 회원 이름/아이디/글/댓글로 게시글 검색 - 작성한 글, 좋아요한 글, 댓글단 글 타임라인 가져오기 기능 구현

--- src/controllers/timeline.ts
+++ src/controllers/timeline.ts
@@ -1,31 +1,178 @@
-import { Connection, getConnection } from "typeorm"
+import { Connection, getConnection, Like, SelectQueryBuilder, Brackets } from "typeorm"
 import Document from "../entities/document"
 
 /* 타임라인 GET */
 export const GetTimeline = async (ctx, next) => {
   const conn: Connection = getConnection()
+  const { page, keyword } = ctx.query
+  const query = Like(`%${keyword}%`)
 
-  try{
-    const timeline: Document[] = await conn
-    .getRepository(Document)
-    .find({
-      order: {
-        id: -1,
-      },
-      relations: [
-        "author",
-        "author.profileImage",
-        "comments",
-        "comments.author",
-        "comments.author.profileImage",
-        "comments.comments",
-        "comments.likedUsers",
-        "likedUsers",
-      ],
-      skip: (ctx.params.page - 1) * 5,
-      take: 5,
-    })
+  try {
+    let queryBuilder: SelectQueryBuilder<Document> = conn
+      .getRepository(Document)
+      .createQueryBuilder('document')
+      .leftJoinAndSelect('document.author', 'author')
+      .leftJoinAndSelect('author.profileImage', 'profileImage')
+      .leftJoinAndSelect('document.comments', 'comments')
+      .leftJoinAndSelect('comments.author', 'commentAuthor')
+      .leftJoinAndSelect('commentAuthor.profileImage', 'commentAuthorProfileImage')
+      .leftJoinAndSelect('comments.comments', 'replies')
+      .leftJoinAndSelect('replies.author', 'repliyAuthor')
+      .leftJoinAndSelect('document.likedUsers', 'likedUsers')
+      .leftJoinAndSelect('comments.likedUsers', 'commentsLikedUsers')
+      .orderBy('document.id', 'DESC')
+      .skip((page - 1) * 5)
+      .take(5)
+    
+    if (keyword) {
+      queryBuilder = queryBuilder
+        .where(new Brackets(qb => {
+          qb.where('author.username = :query', { query })
+          qb.orWhere('commentAuthor.username = :query', { query })
+          qb.orWhere('repliyAuthor.username = :query', { query })
+          qb.orWhere('content = :query', { query })
+          qb.orWhere('comments.content = :query', { query })
+        }))
+    }
 
+    const timeline: Document[] = await queryBuilder.getMany()
+    ctx.body = timeline
+  }
+  catch (e){
+    ctx.throw(400, e)
+  }
+
+  /* GET 완료 응답 */
+  ctx.response.status = 200
+}
+
+export const GetUserTimeline = async (ctx, next) => {
+  const conn: Connection = getConnection()
+  const { username } = ctx.params
+  const { page, keyword } = ctx.query
+  const query = Like(`%${keyword}%`)
+
+  try {
+    let queryBuilder: SelectQueryBuilder<Document> = conn
+      .getRepository(Document)
+      .createQueryBuilder('document')
+      .leftJoinAndSelect('document.author', 'author')
+      .leftJoinAndSelect('author.profileImage', 'profileImage')
+      .leftJoinAndSelect('document.comments', 'comments')
+      .leftJoinAndSelect('comments.author', 'commentAuthor')
+      .leftJoinAndSelect('commentAuthor.profileImage', 'commentAuthorProfileImage')
+      .leftJoinAndSelect('comments.comments', 'replies')
+      .leftJoinAndSelect('replies.author', 'repliyAuthor')
+      .leftJoinAndSelect('document.likedUsers', 'likedUsers')
+      .leftJoinAndSelect('comments.likedUsers', 'commentsLikedUsers')
+      .orderBy('document.id', 'DESC')
+      .skip((page - 1) * 5)
+      .take(5)
+      .where('author.username = :username', { username })
+    
+    if (keyword) {
+      queryBuilder = queryBuilder
+        .andWhere(new Brackets(qb => {
+          qb.where('commentAuthor.username = :query', { query })
+          qb.orWhere('repliyAuthor.username = :query', { query })
+          qb.orWhere('content = :query', { query })
+          qb.orWhere('comments.content = :query', { query })
+        }))
+    }
+
+    const timeline: Document[] = await queryBuilder.getMany()
+    ctx.body = timeline
+  }
+  catch (e){
+    ctx.throw(400, e)
+  }
+
+  /* GET 완료 응답 */
+  ctx.response.status = 200
+}
+
+export const GetLikedTimeline = async (ctx, next) => {
+  const conn: Connection = getConnection()
+  const { username } = ctx.params
+  const { page, keyword } = ctx.query
+  const query = Like(`%${keyword}%`)
+
+  try {
+    let queryBuilder: SelectQueryBuilder<Document> = conn
+      .getRepository(Document)
+      .createQueryBuilder('document')
+      .leftJoinAndSelect('document.author', 'author')
+      .leftJoinAndSelect('author.profileImage', 'profileImage')
+      .leftJoinAndSelect('document.comments', 'comments')
+      .leftJoinAndSelect('comments.author', 'commentAuthor')
+      .leftJoinAndSelect('commentAuthor.profileImage', 'commentAuthorProfileImage')
+      .leftJoinAndSelect('comments.comments', 'replies')
+      .leftJoinAndSelect('replies.author', 'repliyAuthor')
+      .leftJoinAndSelect('document.likedUsers', 'likedUsers')
+      .leftJoinAndSelect('comments.likedUsers', 'commentsLikedUsers')
+      .orderBy('document.id', 'DESC')
+      .skip((page - 1) * 5)
+      .take(5)
+      .where('likedUsers.username = :username', { username })
+    
+    if (keyword) {
+      queryBuilder = queryBuilder
+        .andWhere(new Brackets(qb => {
+          qb.where('author.username = :query', { query })
+          qb.orWhere('repliyAuthor.username = :query', { query })
+          qb.orWhere('content = :query', { query })
+          qb.orWhere('comments.content = :query', { query })
+        }))
+    }
+
+    const timeline: Document[] = await queryBuilder.getMany()
+    ctx.body = timeline
+  }
+  catch (e){
+    ctx.throw(400, e)
+  }
+
+  /* GET 완료 응답 */
+  ctx.response.status = 200
+}
+
+export const GetCommentedTimeline = async (ctx, next) => {
+  const conn: Connection = getConnection()
+  const { username } = ctx.params
+  const { page, keyword } = ctx.query
+  const query = Like(`%${keyword}%`)
+
+  try {
+    let queryBuilder: SelectQueryBuilder<Document> = conn
+      .getRepository(Document)
+      .createQueryBuilder('document')
+      .leftJoinAndSelect('document.author', 'author')
+      .leftJoinAndSelect('author.profileImage', 'profileImage')
+      .leftJoinAndSelect('document.comments', 'comments')
+      .leftJoinAndSelect('comments.author', 'commentAuthor')
+      .leftJoinAndSelect('commentAuthor.profileImage', 'commentAuthorProfileImage')
+      .leftJoinAndSelect('comments.comments', 'replies')
+      .leftJoinAndSelect('replies.author', 'repliyAuthor')
+      .leftJoinAndSelect('document.likedUsers', 'likedUsers')
+      .leftJoinAndSelect('comments.likedUsers', 'commentsLikedUsers')
+      .orderBy('document.id', 'DESC')
+      .skip((page - 1) * 5)
+      .take(5)
+      .where(new Brackets(qb => {
+        qb.where('commentAuthor.username = :username', { username })
+        qb.orWhere('repliyAuthor.username = :username', { username })
+      }))
+    
+    if (keyword) {
+      queryBuilder = queryBuilder
+        .andWhere(new Brackets(qb => {
+          qb.where('author.username = :query', { query })
+          qb.orWhere('content = :query', { query })
+          qb.orWhere('comments.content = :query', { query })
+        }))
+    }
+
+    const timeline: Document[] = await queryBuilder.getMany()
     ctx.body = timeline
   }
   catch (e){
