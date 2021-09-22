af9b317aabe3ff741c7ca4064aa1ba7b56901573
droplet92 <lhs1438@naver.com>
2021-09-22 16:44:38 +0900

    다중 투표 가능 옵션 추가

--- src/entities/vote.ts
+++ src/entities/vote.ts
@@ -6,6 +6,7 @@ import {
   PrimaryGeneratedColumn,
 } from "typeorm"
 import Poll from "./poll"
+import BitTransformer from "../transformer/BitTransformer"
 
 @Entity()
 export default class Vote {
@@ -25,46 +26,54 @@ export default class Vote {
     type: "varchar",
     nullable: false,
   })
-  public title: string  // 투표 제목
+  public title: string          // 투표 제목
 
   @Column({
     name: "end_time",
     type: "datetime",
     nullable: false,
   })
-  public endTime: Date  // 투표 종료 시각
+  public endTime: Date          // 투표 종료 시각
+
+  @Column({
+    name: "has_multiple",
+    type: "bit",
+    transformer: new BitTransformer<boolean>(),
+    default: false,
+  })
+  public hasMultiple: boolean   // 다중 항목 투표 가능 여부
 
   @Column({
     name: "item1",
     type: "varchar",
     nullable: false,
   })
-  public item1: string  // 투표 항목 1
+  public item1: string          // 투표 항목 1
   
   @Column({
     name: "item2",
     type: "varchar",
     nullable: false,
   })
-  public item2: string  // 투표 항목 2
+  public item2: string          // 투표 항목 2
   
   @Column({
     name: "item3",
     type: "varchar",
     nullable: true,
   })
-  public item3: string  // 투표 항목 3
+  public item3: string          // 투표 항목 3
   
   @Column({
     name: "item4",
     type: "varchar",
     nullable: true,
   })
-  public item4: string  // 투표 항목 4
+  public item4: string          // 투표 항목 4
   
   @OneToMany(() => Poll, poll => poll.vote, {
     nullable: false,
   })
-  public polls: Poll[]  // 투표들
+  public polls: Poll[]          // 투표들
 
 }
